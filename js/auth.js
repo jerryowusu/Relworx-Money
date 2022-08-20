d3b00086619360d6c16ef2e7d31cc75b994dbcfa
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-20 02:21:13 +0000

    Fix the bug that is preventing the deployed app to work

--- js/auth.js
+++ js/auth.js
@@ -1,27 +1,29 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable class-methods-use-this */
+/* eslint-disable quotes */
 class Auth {
   constructor() {
-    document.querySelector('body').style.display = 'none';
-    const auth = localStorage.getItem('auth');
+    document.querySelector("body").style.display = "none";
+    const auth = localStorage.getItem("auth");
     this.validateAuth(auth);
   }
 
   validateAuth(auth) {
-    if (auth !== 1) {
-      window.location.replace('/');
+    if (auth != 1) {
+      window.location.replace("/");
     } else {
-      document.querySelector('body').style.display = 'block';
+      document.querySelector("body").style.display = "block";
     }
   }
 
   logOut() {
-    localStorage.removeItem('auth');
-    window.location.replace('/');
+    localStorage.removeItem("auth");
+    window.location.replace("/");
   }
 }
 
 const auth = new Auth();
 
-document.querySelector('.logout').addEventListener('click', () => {
+document.querySelector(".logout").addEventListener("click", () => {
   auth.logOut();
 });
