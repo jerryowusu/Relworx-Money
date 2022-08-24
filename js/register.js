fe49ba8432d805695841aa31cc5a7233a747384b
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-24 09:39:46 +0000

    Create registration page and update login page

---
+++ js/register.js
@@ -0,0 +1,36 @@
+const registerForm = document.querySelector('#register');
+
+const saveDataToLocalStorage = (name, data) => {
+  const userData = [];
+  userData.push(data);
+  if (localStorage[name]) {
+    const store = JSON.parse(localStorage.getItem(name));
+    store.push(data);
+    localStorage.setItem(name, JSON.stringify(store));
+  } else {
+    localStorage.setItem(name, JSON.stringify(userData));
+  }
+};
+
+const saveInputs = (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  const admin = document.querySelector('#admin').checked;
+
+  const userInfo = {
+    name,
+    email,
+    password,
+    admin,
+    balance: 0,
+  };
+
+  saveDataToLocalStorage('userData', userInfo);
+  registerForm.reset();
+  window.location.replace('../home.html');
+};
+
+registerForm.addEventListener('submit', saveInputs);
